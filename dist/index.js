752d617f710e95e8f024253683b33c9ac9499490
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-03-31 16:39:53 +0300

    input project

--- dist/index.js
+++ dist/index.js
@@ -9793,14 +9793,12 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const projectName = core.getInput('project');
+  console.log(projectName);
+  core.debug(process.cwd());
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
