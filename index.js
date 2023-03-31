42d0c37e152aa9f0a6d5179e7e35f75d6bfe388e
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-03-31 17:17:38 +0300

    update

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ import exec from '@actions/exec';
 
 try {
   const projectName = core.getInput('project', { required: true });
-  console.log(projectName);
-  console.log(process.cwd());
-  console.log(process.env);
+  const projectPath = process.cwd();
+  console.log(process.pwd());
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
