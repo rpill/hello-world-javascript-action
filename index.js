c5f8c0a4a605a90580e8e3088359614f00e1fbc2
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-03-31 17:01:18 +0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core';
+import github from '@actions/github';
+import exec from '@actions/exec';
 
 try {
-  const projectName = core.getInput('project');
+  const projectName = core.getInput('project', { required: true });
   console.log(projectName);
-  core.debug(process.cwd());
   console.log(process.cwd());
+  console.log(process.env);
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
