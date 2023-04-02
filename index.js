543fa346eb2d82019a8bd0d7219439131fcf0cf4
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-04-02 13:27:56 +0300

    update

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ import exec from '@actions/exec';
 //     bin: ''
 //   }
 // }
-console.log(1);
-// const runTests = async () => {
-//   try {
-//     const projectName = core.getInput('project', { required: true });
-//     const projectPath = process.cwd();
-//     const cmdOptions = { cwd: projectPath };
-//     await exec.exec(`sh ./bin/${projectName}.sh`, null, cmdOptions);
-//     // Get the JSON webhook payload for the event that triggered the workflow
-//     // const payload = JSON.stringify(github.context.payload, undefined, 2)
-//     // console.log(`The event payload: ${payload}`);
-//   } catch (error) {
-//     core.setFailed(error.message);
-//   }
-// }
 
-// try {
-//   await runTests(params);
-// } catch (e) {
-//   core.error('Tests failed');
-//   throw e;
-// }
+const runTests = async () => {
+  try {
+    const projectName = core.getInput('project', { required: true });
+    console.log(projectName);
+    // const projectPath = process.cwd();
+    // const cmdOptions = { cwd: projectPath };
+    // await exec.exec(`sh ./bin/${projectName}.sh`, null, cmdOptions);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+try {
+  await runTests(params);
+} catch (e) {
+  core.error('Tests failed');
+  throw e;
+}
