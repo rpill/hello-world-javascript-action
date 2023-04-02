89bd528c04fa76ae77c881ddb40232da6dd3d0a5
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-04-02 13:36:05 +0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// import core from '@actions/core';
-// import github from '@actions/github';
+import core from '@actions/core';
+import github from '@actions/github';
 // import exec from '@actions/exec';
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
 
 // const mapping = {
 //   'express-mesto-13': {
@@ -16,7 +13,7 @@ const runTests = async () => {
     const projectName = core.getInput('project', { required: true });
     const projectPath = process.cwd();
     const cmdOptions = { cwd: projectPath };
-    await exec.exec(`sh ./bin/${projectName}.sh`, null, cmdOptions);
+    // await exec.exec(`sh ./bin/${projectName}.sh`, null, cmdOptions);
     // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2)
     // console.log(`The event payload: ${payload}`);
