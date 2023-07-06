06fc3b375146da87cebc7666b129a3bc82d4f285
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-07-06 17:39:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -48,8 +48,8 @@ const prepareProject = async (options) => {
   await exec.exec(copyCmd, null, cmdOptions);
   await io.mkdirP(projectCodePath);
   await io.cp(`${projectPath}/.`, projectCodePath, { recursive: true });
-  await exec.exec('docker', ['build', '--cache-from', '--quiet', projectImageName, '.'], { ...cmdOptions, cwd: projectSourcePath });
-  await exec.exec('docker-compose', ['run', 'app', 'make', 'setup', `PROJECT_NAME=${projectName}`], { ...cmdOptions, cwd: projectSourcePath });
+  await exec.exec('docker', ['build', '--cache-from', projectImageName, '.', '>', '/dev/null', '2>&1'], { ...cmdOptions, cwd: projectSourcePath });
+  await exec.exec('docker-compose', ['run', 'app', 'make', 'setup', `PROJECT_NAME=${projectName}`, '>', '/dev/null', '2>&1'], { ...cmdOptions, cwd: projectSourcePath });
 };
 
 const checkProject = async (options) => {
